2b07bbc85b11d67bb0f64c3768ef8e17efd15c1e
ekfekfdldb <ekfekfdldb@gmail.com>
2025-06-25 10:22:01 +0900

    save

--- JavaScript/early.js
+++ JavaScript/early.js
@@ -1,11 +1,5 @@
 const applyDiscount = (age) => {
-  if (age > 0) {
-    if (age < 20) {
-      return console.log('20% 미성년자 할인이 적용됩니다.');
-    } 
-    return console.log('할인이 적용되지 않습니다.');
-    
-  }
-  return console.log('올바른 나이를 입력해주세요.');
-  
+  if (age <= 0) return console.log('올바른 나이를 입력해주세요.');
+  if (age > 20) return console.log('할인이 적용되지 않습니다.');
+  return console.log('20% 미성년자 할인이 적용됩니다.');
 };
